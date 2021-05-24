4269f66737068c2977ebb2ccf6184dc6d3307960
Hoangtt <ta.tuan.hoang@vsi-international.com>
2021-05-24 14:28:41 +0700

    reuseable logic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,29 +10,16 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import getPosts from "../composables/getPosts";
 import PostList from "../components/PostList.vue";
+
 export default {
     name: "Home",
     components: { PostList },
     setup() {
-        const posts = ref([]);
-        const error = ref(null);
-
-        const load = async () => {
-            try {
-                let data = await fetch("http://localhost:3000/posts");
-                if (!data.ok) {
-                    throw Error("no data available");
-                }
-                posts.value = await data.json();
-            } catch (err) {
-                error.value = err.message;
-            }
-        };
+        const { posts, error, load } = getPosts();
 
         load();
-
         return { posts, error };
     },
 };
